b054c20c5c3627f43d4b44105bb84b19bdc35719
Muhamadasror Abbosov <superpupermegama@gmail.com>
2026-01-19 02:40:35 +0500

    Update app.py

--- app.py
+++ app.py
@@ -73,7 +73,7 @@ if page == "Главная":
     st.title("🛡️ Phishing Detector")
 
     st.image(
-        "https://xakep.ru/wp-content/uploads/2018/03/158954/phishing.jpg",
+        "https://www.istockphoto.com/ru/%D1%84%D0%BE%D1%82%D0%BE/%D0%BA%D0%BE%D0%BD%D1%86%D0%B5%D0%BF%D1%86%D0%B8%D1%8F-%D0%B8%D0%BD%D0%BD%D0%BE%D0%B2%D0%B0%D1%86%D0%B8%D0%B9-%D0%B2-%D0%BE%D0%B1%D0%BB%D0%B0%D1%81%D1%82%D0%B8-%D0%BA%D0%B8%D0%B1%D0%B5%D1%80%D0%B1%D0%B5%D0%B7%D0%BE%D0%BF%D0%B0%D1%81%D0%BD%D0%BE%D1%81%D1%82%D0%B8-%D0%B8%D0%BD%D0%B6%D0%B5%D0%BD%D0%B5%D1%80-%D0%BA%D0%BE%D0%BC%D0%BF%D1%8C%D1%8E%D1%82%D0%B5%D1%80-%D1%80%D0%B0%D0%B1%D0%BE%D1%82%D0%B0%D1%8E%D1%89%D0%B8%D0%B9-%D1%81-gm2035601113-562349611",
         caption="Защита в цифровом мире",
         use_column_width=True
     )
